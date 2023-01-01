9c23d061955be3d781e165c0db2e75caa428fbe1
Nabeel Mohammad Rizwan <nabeel.rizwan123@ymail.com>
2023-01-01 16:24:40 +0530

    added comments

--- Client.py
+++ Client.py
@@ -1,19 +1,19 @@
-import socket
+import socket                                           # import Socket python library
 
-def Client():
-    host='localhost'
+def Client():                                           # Client function
+    host='localhost'                                    # host is localhost
     port=6000
-    socket_client=socket.socket()
-    socket_client.connect((host,port))
+    socket_client=socket.socket()   
+    socket_client.connect((host,port))                  # Connect to Server
 
-    message=input(" -> ")
+    message=input(" -> ")                               # Take input from client
 
-    while message != 'bye':
-        socket_client.send(message.encode())
-        data=socket_client.recv(1024).decode()
-        print(data)
-        message=input(" -> ")
-    socket_client.close()
+    while message != 'bye':                             # While the message from client is not bye, Repeat the loop
+        socket_client.send(message.encode())            # Encode and send the message from client to server
+        data=socket_client.recv(1024).decode()          # Receive the message from Client side and decode
+        print(data)                                     # print the message from Server
+        message=input(" -> ")                           # Take input message from client then repeat loop to send message
+    socket_client.close()                               # if message is bye then close client socket
 
-Client()
+Client()                                                # Call Client function
 
--- Server.py
+++ Server.py
@@ -1,20 +1,20 @@
-import socket
+import socket                                     # import socket python library
 
-def Server():
-    host='localhost'
-    port=6000
+def Server():                                     # Server function
+    host='localhost'                              # host is localhost
+    port=6000                                     # port will be 6000, can be changed
     socket_server=socket.socket()
-    socket_server.bind((host,port))
-    socket_server.listen(2)
-    conn,add=socket_server.accept()
+    socket_server.bind((host,port))               # Bind host and port
+    socket_server.listen(2)                       # listen to 2 connections maximum
+    conn,add=socket_server.accept()               # accept connection
 
-    print("ADDRESS -> "+ str(add))
+    print("ADDRESS -> "+ str(add))                # print address at which connection is established
 
     while True:
-        data=conn.recv(1024).decode()
-        print(data)
-        data=input(" -> ")
-        conn.send(data.encode())
+        data=conn.recv(1024).decode()             # receive data and decode from client
+        print(data)                               # print the data received from client
+        data=input(" -> ")                        # take the input from server
+        conn.send(data.encode())                  # encode and send data to client from server
     conn.close()
 
-Server()
+Server()                                          # Call Server function
